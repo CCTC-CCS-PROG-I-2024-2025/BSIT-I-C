286a4ab9e4006fe150bd5388033b0c4cd4da8dd1
lorejeanpascua <lorejeanpascua@gmail.com>
2024-12-10 10:46:38 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PASCUA_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class PASCUA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 17;
+        int c = 15;
+
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
+    
