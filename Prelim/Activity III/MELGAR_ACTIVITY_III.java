045f13c7bb894ee379762a39b90f18c017c8bacc
Aidezeir <aidezeirpelderosmelgar@gmail.com>
2024-12-10 19:47:41 +0800

    Add files via upload

---
+++ Prelim/Activity III/MELGAR_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class MELGAR_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Aidezeir";
+        int age = 28;
+        double height = 150.5;
+        char surname = 'M';
+        boolean isStudent = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(surname);
+        System.out.println(isStudent);
+    }
+}
