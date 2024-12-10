219d3a2d8eb16f6ceae046b8b020e8a0c6340356
louiesabalands <louiesabalande1177@gmail.com>
2024-12-10 23:18:25 +0800

    Add files via upload

---
+++ Prelim/Activity II/SABALANDE_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+class SABALANDE_ACTIVITY_ll {
+    public static void main (String [] args) {
+        System.out.println("Louie Sabalande");
+        System.out.println(20);
+        System.out.println(18);
+        System.out.println(20-18);
+    }
+}
+
+
