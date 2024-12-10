1b4960410c482b91485be44574757b107e37c1e0
AnjeroSanBarangan <anjerosanbarangan@gmail.com>
2024-12-10 11:27:49 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BARANGAN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class BARANGAN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 9;
+        int c = 6;
+
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
