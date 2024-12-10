cb9594f08bbd4b4b49579e54a2d0b2bb441f5104
MantasJohn <49625643+MantasJohn@users.noreply.github.com>
2024-12-10 10:27:43 +0800

    Add files via upload

---
+++ Prelim/Activity II/BLANCO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class BLANCO_ACTIVITY_ll {
+    public static void main (String [] args) {
+        System.out.println("Christian John Blanco");
+        System.out.println(20);
+        System.out.println(18);
+        System.out.println(20-18);
+    }
+}
---
+++ Prelim/Activity II/BLANCO_ACTIVITY_IV.java
@@ -0,0 +1,16 @@
+public class BLANCO_ACTIVITY_IV {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        int  number= scanner.printInt();
+
+        scanner_close ();
+
+        If (number >= 100);{
+            System.out.println("PERFECT");       
+        }  if (number >= 90);{
+            System.out.println("VERY GOOD");
+       }   if (number >= 80);{
+            System.out.println("GOOD");
+       }
+    }
+}
