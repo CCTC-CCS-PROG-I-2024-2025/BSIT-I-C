b88b1738f7a8277ccea1b0ad34eb72c7f0df69f1
Megan415399 <crongeline@gmail.com>
2024-12-10 10:44:18 +0800

    Add files via upload

---
+++ Prelim/Activity II/CAJUELAN_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class CAJUELAN_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Rongeline Megan B. Cajuelan");
+        System.out.println(18);
+        System.out.println(20);
+        System.out.println(18 - 20);
+    }
+}
