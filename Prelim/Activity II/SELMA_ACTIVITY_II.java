e3b99c9f720d014fcc192df99f652f427e5d1d4a
selmarenelyn <renelynselma1@gmail.com>
2024-12-10 11:31:13 +0800

    Add files via upload

---
+++ Prelim/Activity II/SELMA_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+public class SELMA_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("renelyn selma");
+        System.out.println(19);
+        System.out.println(19);
+        System.out.println(19 - 19);
+    }
+}
+
