0bcf9ddf324cd1573d8d8ba194a6fa7c951e3d34
KienjayMahilum <kienjaymahilum18@gmail.com>
2024-12-10 11:13:59 +0800

    Add files via upload

---
+++ Prelim/Activity II/MAHILUM_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+/**
+ * MAHILUM_ACTIVITY_II
+ */
+public class MAHILUM_ACTIVITY_II {
+
+  public static void main(Stirng[] args)  {
+    System.out.println("Kien Jay Mahilum");
+    System.out.println(18);
+    System.out.println(17);
+    System.out.println(17 - 18);
+  }
+}
