0e38c086d0ce3f06cb6a23f4a5c94157234bef5d
Yehoshuaaaaa <joshuabubuli21@gmail.com>
2024-12-10 10:47:42 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BUBULI_ACTIVITY_IV.java
@@ -0,0 +1,28 @@
+/**
+ * BUBULI_ACTIVITY_IV
+ */
+public class BUBULI_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int z = a+b;
+        System.out.println(z+a);
+
+        int salary = 90000;
+        int tax = 15000;
+        int result = salary - tax;
+        System.out.println(result);
+
+        int stories = 9;
+        int height = 3;
+        int total = stories * height;
+        System.out.println(total);
+
+        int boys = 100;
+        int girls = 5;
+        int all = boys/girls;
+        System.out.println(all);
+
+    }
+}
