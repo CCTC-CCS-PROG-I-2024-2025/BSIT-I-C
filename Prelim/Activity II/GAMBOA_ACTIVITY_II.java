7e98ce9b4579dab405e5c540e567d5e2a61c18f8
KobeMGamboa <kobegamboa59@gmail.com>
2024-12-10 10:38:59 +0800

    Add files via upload

---
+++ Prelim/Activity II/GAMBOA_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class GAMBOA_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Kobe Gamboa" );
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+    }
+    }
