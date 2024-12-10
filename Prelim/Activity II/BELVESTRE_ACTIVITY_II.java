2d1a05fe4310a8f545fdce4ba31df76b67f6b671
jonrobn <johnrobinbelvestre14@gmail.com>
2024-12-11 02:52:37 +0800

    Add files via upload

---
+++ Prelim/Activity II/BELVESTRE_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class BELVESTRE_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("John Robin Belvestre");
+        System.out.println(19);
+        System.out.println(23);
+        System.out.println(23-19);
+    }
+}
