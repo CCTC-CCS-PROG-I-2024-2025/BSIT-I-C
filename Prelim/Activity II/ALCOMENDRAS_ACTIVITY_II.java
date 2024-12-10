78b6f793554e1f5b5388e63d341cd342e078bd75
jenniferalcomendras <jenniferalcomendras8@gmail.com>
2024-12-10 12:10:44 +0800

    Add files via upload

---
+++ Prelim/Activity II/ALCOMENDRAS_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class ALCOMENDRAS_ACTIVITY_II {
+    public static void main(String[] args){
+        System.out.println("Jennifer Alcomendras");
+        System.out.println("18");
+        System.out.println(17);
+        System.out.println(18-17);
+    }    
+}
