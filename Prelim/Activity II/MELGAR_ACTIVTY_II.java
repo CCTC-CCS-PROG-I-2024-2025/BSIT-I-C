ee15285d795446d4d1cd17906caf63415a6d453b
Aidezeir <aidezeirpelderosmelgar@gmail.com>
2024-12-10 19:45:34 +0800

    Add files via upload

---
+++ Prelim/Activity II/MELGAR_ACTIVTY_II.java
@@ -0,0 +1,9 @@
+class MELGAR_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Aidezeir P. MElgar");
+
+        System.out.println(18);
+        System.out.println(20);
+        System.out.println(18-20);
+    }
+}
