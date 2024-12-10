0ae926c766a6782bc7b05e97b0750e629288f131
shirahosh1 <xyriljn@gmail.com>
2024-12-10 10:57:35 +0800

    Add files via upload

---
+++ Prelim/Activity III/DACUYAN_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+/**
+ * DACUYAN_ACTIVITY_III
+ */
+public class DACUYAN_ACTIVITY_III {
+  public static void main(String[] args) {
+    String name = ("Syrell");
+    int age = (20);
+    double height = (160);
+    float weight = (54.8f);
+    char letter = ('S');
+    boolean isStudent = (true);
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(weight);
+    System.out.println(letter);
+    System.out.println(isStudent);
+  }
+}
