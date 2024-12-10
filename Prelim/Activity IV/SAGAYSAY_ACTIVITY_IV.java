5ddfe2c65523965d4431768f5a02e443be7ebf6e
Jansen Sagaysay <jansensagaysay241@gmail.com>
2024-12-10 10:48:22 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SAGAYSAY_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class SAGAYSAY_ACTIVITY_IV {
+    public static void main (String []args){
+        int a = 5;
+        int b = 7;
+        int c = 4;
+        int sum = a+b+c;
+        int difference = a-b-c;
+        int product = a*b*c;
+        int quotient = a/b/c;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+
+
+    }
+}
