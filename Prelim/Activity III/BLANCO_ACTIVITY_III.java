b81dc100af43c80d81a81cc27c334c87b9b61124
MantasJohn <49625643+MantasJohn@users.noreply.github.com>
2024-12-10 11:38:07 +0800

    Add files via upload

---
+++ Prelim/Activity III/BLANCO_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class BLANCO_ACTIVITY_III {
+    public static void main (String[] args){
+        String name = "Christian";
+        int age = 20;
+        double height = 152.4f;
+        char surename = 'B';
+        boolean isnotloyal = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(surename);
+        System.out.println(isnotloyal);
+    }
+}
