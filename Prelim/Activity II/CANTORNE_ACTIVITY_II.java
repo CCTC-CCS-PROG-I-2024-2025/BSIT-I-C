f7d6769aa1c083e6a7d4196a29db1d4c763f7b4a
JOSHuacant1114 <joshuacantorne14@gmail.com>
2024-12-10 11:05:17 +0800

    Add files via upload

---
+++ Prelim/Activity II/CANTORNE_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class CANTORNE_ACTIVITY_II {
+    public static void main(String []args){
+        System.out.println("Joshua Cantorne");
+        System.out.println(19);
+        System.out.println(20);
+        System.out.println(19-20);
+    }
+}
