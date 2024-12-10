557ae7b134a051fe1927f66c9e8ea7fd79faf75b
w1nr3te <charliedecastro12123@gmail.com>
2024-12-10 10:39:28 +0800

    Add files via upload

---
+++ Prelim/Activity III/DE_CASTRO_ACTIVITY_Ill.java
@@ -0,0 +1,18 @@
+class DE_CASTRO_ACTIVITY_III {
+  public static void main(String[] args) {
+    String name = "Charlie";
+    int age = 2;
+    char letter ='A';
+    Float myFloat =68.09f;
+    boolean Student =true;
+    double height =1.8;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(letter);
+    System.out.println(myFloat);
+    System.out.println(Student);
+    System.out.println(height);
+  }
+
+}
