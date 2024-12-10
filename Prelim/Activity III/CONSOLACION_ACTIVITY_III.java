7640ab9d3bb9ab62094e33cafa16676b2ad9511f
SandraMaeConsolacion <sandramaeconsolacion3@gmail.com>
2024-12-10 11:22:50 +0800

    Add files via upload

---
+++ Prelim/Activity III/CONSOLACION_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class CONSOLACION_ACTIVITY_III{
+    public static void main (String[]args){
+        String name ="Sandra";
+        int age = 18;
+        double height =4.0;
+        float weight = 45.0f;
+        char letter = 's';
+        boolean isStudent = true;
+
+       System.out.println(name);
+       System.out.println(age);
+       System.out.println(height);
+       System.out.println(weight);
+       System.out.println(letter);
+       System.out.println(isStudent);
+    }
+}
