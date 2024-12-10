76eec737b0ee3b6f1c2c3838e13058cc3814aa93
Alyanah Belle Bacunawa <alyanahbelleonobacunawa@gmail.com>
2024-12-10 11:30:07 +0800

    Add files via upload

---
+++ Prelim/Activity II/BACUNAWA_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+class BACUNAWA_ACTIVITY_II {
+    public static void main (String[] args) {
+        System.out.println("Alyanah Belle Ono Bacunawa");
+        System.out.println(17);
+        System.out.println(21);
+        System.out.println(17 - 21);
+
+
+    }
+}
