a86296d4d5a314497e910cd888447e5ccfb3cf05
Karess Sheen Canillo <karesssheenc@gmail.com>
2024-12-09 18:47:56 -0800

    Add files via upload

---
+++ Prelim/Activity IV/CANILLO_ACTIVITY_IV.java
@@ -0,0 +1,16 @@
+public class CANILLO_ACTIVITY_IV {
+    
+    public static void main(String[] args) {
+        int girls = 10;
+        int boys = 13;
+        int special = 5;
+        int sum = girls + boys + special;
+        int def = girls - boys - special;
+        int pro = girls*boys*special;
+        int qua = girls/boys/special;
+        System.out.println(sum);
+        System.out.println(def);
+        System.out.println(pro);
+        System.out.println(qua);
+    }
+}
