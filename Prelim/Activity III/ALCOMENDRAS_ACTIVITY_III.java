11deb6b028a64bc15cc2026af391e7a1a51c7a56
jenniferalcomendras <jenniferalcomendras8@gmail.com>
2024-12-10 12:11:03 +0800

    Add files via upload

---
+++ Prelim/Activity III/ALCOMENDRAS_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+public class ALCOMENDRAS_ACTIVITY_III {
+    public static void main(String[] args){
+        String name = "Jennifer";
+        int age = 18;
+        double height = 5.1;
+        float weight = 40.1f;
+        char letter = 'A';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+    }
+}
