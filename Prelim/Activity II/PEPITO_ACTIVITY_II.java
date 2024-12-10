be1b4b490c754534984605cb266ef6baf94a5f11
John Anthony Pepito <john.anthony789456@gmail.com>
2024-12-10 11:01:03 +0800

    Add files via upload

---
+++ Prelim/Activity II/PEPITO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class PEPITO_ACTIVITY_II {
+    public static void main (String[] args) {
+        System.out.println("John Anthony Castro Pepito");
+        System.out.println(17);
+        System.out.println(21);
+        System.out.println(17 - 21);
+    }
+}
