bb359aa38e3e3f4f9411badec4b5eb89d1c80b87
Alyanah Belle Bacunawa <alyanahbelleonobacunawa@gmail.com>
2024-12-10 11:30:40 +0800

    Add files via upload

---
+++ Prelim/Activity III/BACUNAWA_ACTIVITY_III.java
@@ -0,0 +1,23 @@
+class BACUNAWA_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        String name;
+        name = "Alyanah";
+        int age = 18;
+        double height = 150;
+        float weight = 45f;
+        char letter = 'A';
+        Boolean isStudent = false; 
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.pritln(weight);
+        System.out.println(letter);
+        System.out.println(isStudent);
+
+
+
+
+    }
+}
