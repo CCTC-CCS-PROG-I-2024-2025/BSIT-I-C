dad86f7249ae0760344b8453f6a3d9b024d2debd
Jerica Mahinay <jericaledesma29@gmail.com>
2024-12-10 10:42:39 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MAHINAY_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class MAHINAY_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        int c = 9;
+        int sum = a + b + c;
+        int difference = a - b - c;
+        int product = a * b * c;
+        int quotient = a / b / c;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+
+    }
+
+}
