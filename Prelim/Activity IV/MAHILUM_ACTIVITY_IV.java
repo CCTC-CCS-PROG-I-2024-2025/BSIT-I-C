7543c2ae9be06f4fcb2eafe57b69451fabc6babe
KienjayMahilum <kienjaymahilum18@gmail.com>
2024-12-10 11:15:18 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MAHILUM_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+public class MAHILUM_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int k = 5;
+        int b = 6;
+        int f = 3;
+
+        int sum = k+b+f;
+        int difference = k-b-f;
+        int product = k*b*f;
+        int qoutient = k/b/f;
+        
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(qoutient);
+
+    }
+
+    
+}
