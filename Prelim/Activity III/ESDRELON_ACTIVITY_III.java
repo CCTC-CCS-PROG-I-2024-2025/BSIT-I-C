b9a8951f001488977f379519ff375551c2c3c49d
Rhylle1 <esdrelonrhylle@gmail.com>
2024-12-10 11:00:50 +0800

    Add files via upload

---
+++ Prelim/Activity III/ESDRELON_ACTIVITY_III.java
@@ -0,0 +1,24 @@
+ class ESDRELON_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        String name = "rhylle";
+        int age =19;
+        double height =1.5;
+        Float Weight =62.4f;
+        char letter = 'R';
+        boolean isstudent =false; 
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(Weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+        
+
+      
+
+    
+    
+    }
+}
