9f40c24852a08c146f1fbb729dac076124415595
stevendumasig11 <stevendumasig11@gmail.com>
2024-12-10 11:37:13 +0800

    Add files via upload

---
+++ Prelim/Activity III/DUMASIG_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+public class DUMASIG_ACTIVITY_III{
+
+    public static void main (String []args) {
+        String name = "STEVEN";
+        System.out.print (name);
+        int age = 24;
+        System.out.println(age);
+        char letter = 'S';
+        System.out.println(letter);
+        boolean isOpen = false;
+        System.out.println(isOpen);
+        double weight = 65;
+        System.out.println(weight);
+        float height = 167f;
+        System.out.println(height);
+
+    }
+}
+
+
