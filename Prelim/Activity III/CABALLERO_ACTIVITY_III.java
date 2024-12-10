aa3235bc84fe04f77b846fd3fb8252cbbd6770e6
Marian Rose Caballero <caballeromarianrose2005@gmail.com>
2024-12-10 10:58:49 +0800

    Add files via upload

---
+++ Prelim/Activity III/CABALLERO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class CABALLERO_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = ("Marian Rose Caballero");
+        System.out.println(name);
+        int score = (25);
+        System.out.println(score);
+        double height = (154.94);
+        System.out.println(height);
+        float price = (154.94f);
+        System.out.println(price);
+        char answer = ('B');
+        System.out.println(answer);
+        Boolean open = (true);
+        System.out.println(open);
+    
+    }
+}
