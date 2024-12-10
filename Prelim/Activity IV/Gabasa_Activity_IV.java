c58f348f66eca7d03878073c519d6f0969e9e229
Gabasa, Marben T. <118230873+feiro18@users.noreply.github.com>
2024-12-10 11:03:05 +0800

    Add files via upload

---
+++ Prelim/Activity IV/Gabasa_Activity_IV.java
@@ -0,0 +1,24 @@
+public class Gabasa_Activity_IV {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        int c = 20; 
+
+        int sum = a+b+c;
+        int product = a*b*c;
+        int difference = a-b-c;
+        int quotient = a/b/c;
+        int total = sum+product+difference+quotient;
+
+        System.out.println("  R   E   S   U   L   T   S   ");
+        System.out.println();
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+        System.out.println();
+        System.out.println("Total = " + total);
+        System.out.println();
+    }
+    
+}
