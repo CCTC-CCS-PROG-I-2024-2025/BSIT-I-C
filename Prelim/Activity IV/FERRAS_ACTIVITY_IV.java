c1dac5456d3d1c20df8b368759a5d5edd9ca23fd
ferras11 <rheabieferras@gmail.com>
2024-12-10 11:20:48 +0800

    Add files via upload

---
+++ Prelim/Activity IV/FERRAS_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class FERRAS_ACTIVITY_IV { 
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 4;
+        int sum = a+b+c;
+        int difference = a-b-c;
+        int product = a*b*c;
+        int qoutient = a/b/c;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(qoutient);
+        
+    }
+    
+}
