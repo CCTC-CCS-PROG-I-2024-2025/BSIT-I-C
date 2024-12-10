d7371ac5183dafdcac0f7b7e1945d1c3db249791
joshua030905 <avilesjoshua210@gmail.com>
2024-12-10 11:06:58 +0800

    Add files via upload

---
+++ Prelim/Activity II/AVILES_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+/**
+ * AVILES_ACTIVITY_II
+ */
+public class AVILES_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        System.out.println("joshua aviles");
+        System.out.println(19);
+        System.out.println(19);
+        System.out.println(19 - 19);
+    }
+}
