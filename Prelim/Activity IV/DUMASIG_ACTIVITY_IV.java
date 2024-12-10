f43be140a33f46975bc3c87cb43ce254ff4e1bea
stevendumasig11 <stevendumasig11@gmail.com>
2024-12-10 11:38:12 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DUMASIG_ACTIVITY_IV.java
@@ -0,0 +1,24 @@
+public class DUMASIG_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 5;
+        int c = a+b;
+    
+        int sum = (a+b);
+        int total =(c-a);
+        int result= (a*b);
+        int res = (c/a);
+    
+        System.out.println(sum);
+        System.out.println(total);
+        System.out.println(result);
+        System.out.println(res);
+    
+    }
+   
+
+
+
+
+    
+}
