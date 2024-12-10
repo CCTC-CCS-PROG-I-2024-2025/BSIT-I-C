8e686a4677c27d14a05745ee0fa213dd48c77678
kuzeeexyz <junrelterobias123@gmail.com>
2024-12-10 10:44:00 +0800

    Add files via upload

---
+++ Prelim/Activity III/TEROBIAS_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+public class TEROBIAS_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Junrel";
+        int age = 19;
+        double height = 175.5;
+        float weight = 50.0f;
+        char letter = 'p';
+        boolean isStudent = false;
+    
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isStudent);
+        
+    
+    }
+        
+}
