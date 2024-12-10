d9a045c9da5938a3b0978290640914fc5445b116
Karess Sheen Canillo <karesssheenc@gmail.com>
2024-12-09 18:56:01 -0800

    Add files via upload

---
+++ Prelim/Activity III/CANILLO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class CANILLO_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        String name = "karess sheen canillo";
+        int age = 18;
+        double income = 150.90;
+        char middle = 'L';
+        boolean friendly = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(income);
+        System.out.println(middle);
+        System.out.println(friendly);
+
+
+    }
+}
