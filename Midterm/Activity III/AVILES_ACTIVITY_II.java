e99a82c5dd2d47b563000dbc1a3eafc94ec7cc18
joshua030905 <avilesjoshua210@gmail.com>
2024-12-10 11:54:55 +0800

    Add files via upload

---
+++ Midterm/Activity III/AVILES_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AVILES_ACTIVITY_II {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        while(num>=0){
+            System.out.println(num);
+            num--;
+        }
+    }
+}
