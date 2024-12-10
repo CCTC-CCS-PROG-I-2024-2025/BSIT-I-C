87913adcfb06b3d7bfdd24721282ee80884154a5
Karess Sheen Canillo <karesssheenc@gmail.com>
2024-12-09 19:13:47 -0800

    Add files via upload

---
+++ Midterm/Activity III/CANILLO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+package MIDTERM_CANILLO;
+import java.util.Scanner;
+ 
+public class CANILLO_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int num = sc.nextInt();
+        
+        while(num>=0) {
+            System.out.println(num);
+            num--;
+        }
+    }
+}
