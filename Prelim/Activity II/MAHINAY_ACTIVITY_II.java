069c6e978bdd7989cb3ce9e83cb44c7906caeb41
khattyybanez <ybanezkhatty1@gmail.com>
2024-12-10 11:06:51 +0800

    Add files via upload

---
+++ Prelim/Activity II/MAHINAY_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+class MAHINAY_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Aliah Beth Mahinay");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+
+    }
+
+}
