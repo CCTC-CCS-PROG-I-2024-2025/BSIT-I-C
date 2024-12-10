2300b626423753b327a9910d1933bdaf91d5c932
JOSHuacant1114 <joshuacantorne14@gmail.com>
2024-12-10 11:13:18 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CANTORNE_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class CANTORNE_ACTIVITY_IV {
+    public static void main(String [] args){
+        int x = 7;
+        int z = 3;
+        int p = 10;
+        
+        int sum = x+z+p;
+        int diff = x-z-p;
+        int prod = x*z*p;
+        int qoute = x/z/p;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qoute);
+    }
+}
