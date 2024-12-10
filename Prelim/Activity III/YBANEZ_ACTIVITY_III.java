197a11e1f3d306627cee6fd405bf8f57227b8b28
selmarenelyn <renelynselma1@gmail.com>
2024-12-10 11:55:42 +0800

    Add files via upload

---
+++ Prelim/Activity III/YBANEZ_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+public class SELMA_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name="Renelyn";
+        int age=18;
+        double height=1.49;
+        char letter='A'; 
+        boolean isStudent=true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isStudent);
+    }
+}
