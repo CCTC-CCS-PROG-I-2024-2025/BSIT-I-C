3f0140d49bba1c2543220b490e0293a196dd9f4e
John Anthony Pepito <john.anthony789456@gmail.com>
2024-12-10 10:43:34 +0800

    Create  PEPITO_ACTIVITYII.java

---
+++ PEPITO_ACTIVITYII.java
@@ -0,0 +1,8 @@
+class PEPITO_ACTIVITY_II {
+    public static void main (String[] args) {
+        System.out.println("JOHN ANTHONY PEPITO");
+        System.out.println(21);
+        System.out.println(17);
+        System.out.println(21 - 17);
+    }
+} 
