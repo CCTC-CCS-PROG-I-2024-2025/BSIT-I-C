17a2587f7c205c09525f817edafcfe51d4db2860
jesrylfaith102006 <faithjesrylg@gmail.com>
2024-12-10 11:32:10 +0800

    Add files via upload

---
+++ Prelim/Activity III/GADIANO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class GADIANO_ACTIVITY_III {
+public static void main(String[] args) {
+    String name = "Jesryl Faith Gadiano";
+    int ages = 19;
+    float height = 40.2f;
+    char letter = 'C';
+    boolean isClose = true;
+    System.out.println(name);
+    System.out.println(ages);
+    System.out.println(height);
+    System.out.println(letter);
+    System.out.println(isClose);
+
+}
+
+    }
+
