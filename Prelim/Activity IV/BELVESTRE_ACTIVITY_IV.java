08f7326e3c25d43229ed112ea2cbc19349a22046
jonrobn <johnrobinbelvestre14@gmail.com>
2024-12-11 02:56:16 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BELVESTRE_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class BELVESTRE_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 35;
+        int c = 50;
+
+        int sum = a+b+c;
+        int difference = a - b - c;
+        int product = a * b * c;
+        int quotient = a / b / c;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+    
+}
