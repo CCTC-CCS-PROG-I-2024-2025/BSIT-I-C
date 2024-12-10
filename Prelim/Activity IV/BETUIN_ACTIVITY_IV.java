fe8d6ad28169d141b775b519e1bffc947108d780
LYREL N BETUIN <lyrelbetuin@gmail.com>
2024-12-10 10:56:03 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BETUIN_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+public class BETUIN_ACTIVITY_IV {
+    public static void main(String[] args) {
+                int age = 2;       
+                int month  = 1;       
+                int year = 4;   
+                int sum = age + month + year;
+                int diff = age - year;
+                int prod = year * month;
+                int quot = month / age;
+                System.out.println(sum);          
+                System.out.println(diff);    
+                System.out.println(prod); 
+                System.out.println(quot);          
+            }
+        }
