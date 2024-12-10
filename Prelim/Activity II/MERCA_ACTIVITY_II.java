42f54648d73cafe243d1e4f098f62ec2a18d6ea6
pyangmerca <piamerca88@gmail.com>
2024-12-10 10:46:56 +0800

    Add files via upload

---
+++ Prelim/Activity II/MERCA_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class MERCA_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Zopia Mae V. Merca");
+        System.out.println(18);
+        System.out.println(20);
+        System.out.println(18 - 20);
+    }
+}
