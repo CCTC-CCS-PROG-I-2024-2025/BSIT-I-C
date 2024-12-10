15252090c54e983f6b44111201716bde6eb5c1a9
shirahosh1 <xyriljn@gmail.com>
2024-12-10 10:56:42 +0800

    Add files via upload

---
+++ Prelim/Activity II/DACUYAN_ACTIVITY_II.java
@@ -0,0 +1,13 @@
+/**
+ * DACUYAN_ACTIVITY_II
+ */
+public class DACUYAN_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Syrell Jane Dacuyan");
+        System.out.println(20);
+        System.out.println(22);
+        System.out.println(20 - 22);
+
+    }
+
+}
