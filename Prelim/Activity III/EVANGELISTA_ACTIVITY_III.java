7c843776559a4d75b0a9fbf1717f0d094feb1121
johnEvangelista <evangelistajohn356@gmail.com>
2024-12-10 10:57:50 +0800

    Add files via upload

---
+++ Prelim/Activity III/EVANGELISTA_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class EVANGELISTA_ACTIVITY_III {
+    public static void main (String[] args){
+        String name = "JOHN";
+        int age = 20;
+        double height = 152.4f;
+        char surename = 'B';
+        boolean isnotloyal = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(surename);
+        System.out.println(isnotloyal);
+    }
+}
