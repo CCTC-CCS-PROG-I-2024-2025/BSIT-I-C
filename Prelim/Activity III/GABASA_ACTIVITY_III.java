c107ed4bab67761e12d75931005c8754b993e0f5
Gabasa, Marben T. <118230873+feiro18@users.noreply.github.com>
2024-12-10 11:00:12 +0800

    Add files via upload

---
+++ Prelim/Activity III/GABASA_ACTIVITY_III.java
@@ -0,0 +1,21 @@
+    class GABASA_ACTIVITY_III{
+    public static void main(String[] args) {
+        String name = "Marben";
+        int age = 18;
+        boolean isStudent = true;
+        char letter = 'C';
+        float weight = 43.1f;
+        double height = 152.1;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isStudent);
+        System.out.println(letter);
+        System.out.println(weight);
+        System.out.println(height);
+    }
+}
+        
+
+
+
