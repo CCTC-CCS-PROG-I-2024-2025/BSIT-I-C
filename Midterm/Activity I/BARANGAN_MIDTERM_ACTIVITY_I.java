bf4729d2fb7a678c3430e7bc6390a1c0aee1c5e7
AnjeroSanBarangan <anjerosanbarangan@gmail.com>
2024-12-10 11:39:31 +0800

    Add files via upload

---
+++ Midterm/Activity I/BARANGAN_MIDTERM_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+public class BARANGAN_MIDTERM_ACTIVITY_I {
+    import java.util.Scanner;
+class Barangan_activity_I {
+    public static void main(String [ ] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println(x: "Enter the color of the item: ");
+        String color = "green";
+
+        int color = "green";
+        
+        switch(color) {
+        case 1:
+            System.out.println("red");
+            break;
+        case 2:
+            System.out.println("green");
+            break;
+        case 3:
+            System.out.println("black"):
+            break;
+        }
+    }
+}
+}
