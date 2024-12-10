52772105d0209bb6dcb01a65a70933e419cea2cf
w1nr3te <charliedecastro12123@gmail.com>
2024-12-10 10:39:59 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DE_CASTRO_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+class DE_CASTRO_ACTIVITY_IV {
+  public static void main(String[] args) {
+    int studentage = 20;
+    int Month = 8;
+    int favnum =36;
+
+    int sum = studentage + Month + favnum;
+    int diff = studentage - Month - favnum;
+    int quot = studentage / Month / favnum;
+    int prod = studentage * Month * favnum;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(quot);
+    System.out.println(prod);
+
+    
+
+  }
+}
