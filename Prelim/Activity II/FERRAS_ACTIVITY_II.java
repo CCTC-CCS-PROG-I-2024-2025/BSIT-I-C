56213b6a2211ccc00d43a6d2ed838346ebb0cd6a
ferras11 <rheabieferras@gmail.com>
2024-12-10 11:19:36 +0800

    Add files via upload

---
+++ Prelim/Activity II/FERRAS_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class FERRAS_ACTIVITY_II { 
+    public static void main (String[] args) {
+        System.out.println("Rheabie Ferras");
+        System.out.println(17);
+        System.out.println(18);
+        System.out.println(17-18);
+
+    }
+}
