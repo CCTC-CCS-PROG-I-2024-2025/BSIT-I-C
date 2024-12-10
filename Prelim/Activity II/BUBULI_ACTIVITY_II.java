e32a593f119a1e87afebaa332a07be9afb0c3169
Yehoshuaaaaa <joshuabubuli21@gmail.com>
2024-12-10 10:46:39 +0800

    Add files via upload

---
+++ Prelim/Activity II/BUBULI_ACTIVITY_II.java
@@ -0,0 +1,7 @@
+    public class BUBULI_ACTIVITY_II {
+ public static void main(String[] args) {
+        System.out.println("Joshua");
+        System.out.println(18);
+        System.out.println(18-18);
+    }
+}
