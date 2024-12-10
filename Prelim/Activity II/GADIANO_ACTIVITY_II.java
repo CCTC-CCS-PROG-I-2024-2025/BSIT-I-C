2ca5533baddd543046e35aef5205281c5d7ac1c7
jesrylfaith102006 <faithjesrylg@gmail.com>
2024-12-10 11:31:42 +0800

    Add files via upload

---
+++ Prelim/Activity II/GADIANO_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+/**
+ * GADIANO_ACTIVITY_II
+ */
+public class GADIANO_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        System.out.println("Jesryl Faith Gadiano");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);  
+    }
+}
