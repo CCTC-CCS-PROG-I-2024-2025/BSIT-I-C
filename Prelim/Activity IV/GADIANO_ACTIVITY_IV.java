3bb980cb3638c004bfe8ecb91133034dc151252b
jesrylfaith102006 <faithjesrylg@gmail.com>
2024-12-10 11:32:45 +0800

    Add files via upload

---
+++ Prelim/Activity IV/GADIANO_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class GADIANO_ACTIVITY_IV {
+    public static void main(String[] args) {
+       int y = 19;
+       int x = 15;
+       int b = 10;
+       int sum = y+x+b;
+       int difference = x-y-b;
+       int product = x*b*y;
+       int quatient = x/b/y;
+       System.out.println(sum);
+       System.out.println(difference);
+       System.out.println(product);
+       System.out.println(quatient);
+
+    
+    }
+}
