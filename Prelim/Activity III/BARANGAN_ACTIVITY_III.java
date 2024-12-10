2ce867fa302506b78bd53941689c14391c71e92b
AnjeroSanBarangan <anjerosanbarangan@gmail.com>
2024-12-10 11:27:34 +0800

    Add files via upload

---
+++ Prelim/Activity III/BARANGAN_ACTIVITY_III.java
@@ -0,0 +1,12 @@
+public class BARANGAN_ACTIVITY_III {
+    public static void main (String[] args) {
+        String name = "Angelo";
+        System.out.println(name);
+      String color = "blue";
+      System.out.println(color);
+      int age = 19;
+      System.out.println(age);  
+      float height = 157.48f;
+      System.out.println(height);
+    }
+}
