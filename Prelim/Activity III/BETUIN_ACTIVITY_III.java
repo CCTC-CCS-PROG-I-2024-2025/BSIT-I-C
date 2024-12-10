01d9f84d904d1f97a8827662e028a0fa4b53b9c4
LYREL N BETUIN <lyrelbetuin@gmail.com>
2024-12-10 10:53:07 +0800

    Add files via upload

---
+++ Prelim/Activity III/BETUIN_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+public class BETUIN_ACTIVITY_III {
+    
+               public static void main(String[] args) {
+                    
+           int age = 25;
+           double temperature = 98.6;
+           char initial = 'A';
+           boolean isJavaFun = true;
+           String greeting = "Hello, World!";
+           
+              System.out.println(age);
+              System.out.println(temperature);
+              System.out.println(initial);
+              System.out.println(isJavaFun);
+              System.out.println(greeting);
+               }
+   
+            }      
+   
+        
