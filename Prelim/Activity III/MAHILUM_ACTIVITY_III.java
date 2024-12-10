0b01a04835fb8763b390e1949cefa20f8c32940b
KienjayMahilum <kienjaymahilum18@gmail.com>
2024-12-10 11:14:45 +0800

    Add files via upload

---
+++ Prelim/Activity III/MAHILUM_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+public class MAHILUM_ACTIVITY_III {
+public static void main(String[] args) {
+    String name = "kien jay";
+    int age = 23;
+    double height = 157;
+    float weight = 40;
+    char letter = 'K';
+    boolean isStudent = true;
+    
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(weight);
+    System.out.println(letter);
+    System.out.println(isStudent);
+}
+
+}
