bf10894ab8185ae89eac5952870beffc737bd9cc
ferras11 <rheabieferras@gmail.com>
2024-12-10 11:20:22 +0800

    Add files via upload

---
+++ Prelim/Activity III/FERRAS_ACTIVITY_III.java
@@ -0,0 +1,12 @@
+public class FERRAS_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name="Claire";
+        int age=17;
+        double height=1.49;
+        char letter='A';
+        boolean isStudent=true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isStudent);
