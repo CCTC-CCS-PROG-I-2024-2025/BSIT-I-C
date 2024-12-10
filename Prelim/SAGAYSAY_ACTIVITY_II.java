53b0fca4f44bf01abf652abd2f1063e69ff58455
Jansen Sagaysay <jansensagaysay241@gmail.com>
2024-12-10 10:45:45 +0800

    Add files via upload

---
+++ Prelim/SAGAYSAY_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class SAGAYSAY_ACIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("jansen sagaysay");
+        System.out.println(18);
+        System.out.println(28);
+        System.out.println(28-18);
+    }
+}
