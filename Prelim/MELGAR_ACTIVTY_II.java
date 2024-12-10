996a8e6ebbce49f8f7d332e91308e5f2e150014c
Aidezeir <aidezeirpelderosmelgar@gmail.com>
2024-12-10 19:43:53 +0800

    Delete Prelim/MELGAR_ACTIVTY_II.java

--- Prelim/MELGAR_ACTIVTY_II.java
+++
@@ -1,9 +0,0 @@
-class MELGAR_ACTIVITY_II {
-    public static void main(String[] args) {
-        System.out.println("Aidezeir P. MElgar");
-
-        System.out.println(18);
-        System.out.println(20);
-        System.out.println(18-20);
-    }
-}
