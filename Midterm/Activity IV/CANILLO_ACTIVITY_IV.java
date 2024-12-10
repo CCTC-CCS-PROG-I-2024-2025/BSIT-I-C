cd6a84ec08f293c7a3843cf63bbd2cb1880e93df
Karess Sheen Canillo <karesssheenc@gmail.com>
2024-12-09 19:14:18 -0800

    Add files via upload

---
+++ Midterm/Activity IV/CANILLO_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+package MIDTERM_CANILLO;
+import java.util.Scanner;
+
+public class CANILLO_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a non-negative integer:");
+        int x = sc.nextInt();
+
+        int factorial = 1;
+
+        for ( int j=1; j<=x; j++){
+            factorial *=j ;
+        }
+         System.out.println("the factorial of" + x + "is:" + factorial);
+
+
+    }
+}
