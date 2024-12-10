97ba53beb7c8e0e932e0a394e1af945238df5d81
John Anthony Pepito <john.anthony789456@gmail.com>
2024-12-10 11:04:30 +0800

    Add files via upload

---
+++ Prelim/Activity III/PEPITO_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+class PEPITO_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name;
+        name = "Anthony";
+        int age = 23;
+        double height = 178;
+        float weight = 49f;
+        char letter = 'j';
+        Boolean isStudent = false;
+
+         System.out.println(name);
+         System.out.println(age);
+         System.out.println(height);
+         System.out.println(weight);
+         System.out.println(letter);
+         System.out.println(isStudent);
+
+    }
+
+}
