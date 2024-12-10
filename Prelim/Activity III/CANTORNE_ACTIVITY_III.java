b8156c4f965df0d15b972b16848ff380639a734e
JOSHuacant1114 <joshuacantorne14@gmail.com>
2024-12-10 11:17:55 +0800

    Add files via upload

---
+++ Prelim/Activity III/CANTORNE_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class CANTORNE_ACTIVITY_III {
+    public static void main(String [] args){
+        String name = "Joshua";
+        int age = 19;
+        double height = 5.4;
+        float weight = 60f;
+        char letter = 'M';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+        
+    }
+}
