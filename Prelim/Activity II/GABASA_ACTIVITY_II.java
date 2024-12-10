04b70c562364b8d97e1d0ed12731c6a958f592d5
Gabasa, Marben T. <118230873+feiro18@users.noreply.github.com>
2024-12-10 10:51:39 +0800

    Add files via upload

---
+++ Prelim/Activity II/GABASA_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+public class GABASA_ACTIVITY_II {
+    
+    public static void main(String[] args) {
+        System.out.println("Marben");
+        System.out.println(18);
+        System.out.println(20);
+        System.out.println(18-20);
+    }
+        
+    }
