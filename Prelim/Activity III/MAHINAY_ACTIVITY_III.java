2b2ece853a1c6a9cb885655a9baaa76b17777c5f
aliahbethmahinay <aliahbethmahinay@gmail.com>
2024-12-10 11:35:01 +0800

    Add files via upload

---
+++ Prelim/Activity III/MAHINAY_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class MAHINAY_ACTIVITY_III {
+    public static void main(String[] args) {
+        String color = "Green";
+        int age = 18;
+        double height = 1.49;
+        char letter = 'A';
+        boolean isOpen = true;
+        System.out.println(color);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+    
+}
