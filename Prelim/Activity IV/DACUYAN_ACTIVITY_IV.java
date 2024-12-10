743878e20078fd20208a7095af5232bb8ced6a5c
shirahosh1 <xyriljn@gmail.com>
2024-12-10 10:58:17 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DACUYAN_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class DACUYAN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int e = 10;
+        int d = 5;
+        int c = 15;
+        
+        int sum = e + d + c;
+        int diff = e - d - c;
+        int prod = e * d * c;
+        int quot = e / d / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+    }
+}
