709e700d2e40617e9300ba8b3a0199063c9a282a
Lovely2971 <jamesdavefajardo86@gmail.com>
2024-12-10 10:52:51 +0800

    Add files via upload

---
+++ Prelim/Activity IV/FAJARDO_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class FAJARDO_ACTIVITY_IV {}
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 6;
+        
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+}
