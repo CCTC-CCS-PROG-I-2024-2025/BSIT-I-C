22f594ec51628f0e003b4b89d800f7abddbce299
stevendumasig11 <stevendumasig11@gmail.com>
2024-12-10 11:35:51 +0800

    Add files via upload

---
+++ Prelim/Activity II/DUMASIG_ACTIVITY_ll.java
@@ -0,0 +1,8 @@
+class DUMASIG_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Dumasig Steven");
+        System.out.println(24);
+        System.out.println(20);
+        System.out.println(24-20);
+    }
+}    
