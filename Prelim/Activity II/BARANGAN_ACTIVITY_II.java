9db2b796f3d4d284b30df135bf915537273be354
AnjeroSanBarangan <anjerosanbarangan@gmail.com>
2024-12-10 11:27:00 +0800

    Add files via upload

---
+++ Prelim/Activity II/BARANGAN_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+public class BARANGAN_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Angelo Barangan");
+        System.out.println(19);
+        System.out.println(18);
+        System.out.println(19-18);
+    }
+    }
+
