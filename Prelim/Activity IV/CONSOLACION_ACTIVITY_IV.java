a5d7cadc72e1d8e208dd4fd41c705685ab4d5360
SandraMaeConsolacion <sandramaeconsolacion3@gmail.com>
2024-12-10 11:30:53 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CONSOLACION_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class CONSOLACION_ACTIVITY_IV{
+    public static void main(String [] args){
+        int  a = 6;
+        int b = 12;
+        int c = 8;
+
+        int sum = a+b+c;
+        int diff = a-b-c;
+        int prod = a*b*c;
+        int qoute = a/b/c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qoute);
+    }
+}
