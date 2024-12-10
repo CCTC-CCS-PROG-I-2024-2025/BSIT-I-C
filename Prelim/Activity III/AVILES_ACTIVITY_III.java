f71f9e2c95ef433a1fa407e0cbdcdb1a9e2275e8
joshua030905 <avilesjoshua210@gmail.com>
2024-12-10 11:07:42 +0800

    Add files via upload

---
+++ Prelim/Activity III/AVILES_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+/**
+ * AVILES_ACTIVITY_III
+ */
+public class AVILES_ACTIVITY_III {
+    public static void main(String[] args){
+        String name = "joshua";
+        int age = 19;
+        double height = 5.4;
+        float weight = 60f;
+        char letter = 'j';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+    }
+}
