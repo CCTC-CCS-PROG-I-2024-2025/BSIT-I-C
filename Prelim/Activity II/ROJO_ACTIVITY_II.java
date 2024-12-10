5729c325fbfabd0ff82b50b220c12934dd3a736d
tonitombits <junrelterobias6@GMAIL.COM>
2024-12-10 11:34:36 +0800

    Add files via upload

---
+++ Prelim/Activity II/ROJO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+public class ROJO_ACTIVITY_II { 
+
+    public static void main(String[] args) {
+        System.out.println("Lyrel N. Betuin");  
+        System.out.println("18");               
+        System.out.println("19");              
+        System.out.println(19-18);                          
+    } 
+}
