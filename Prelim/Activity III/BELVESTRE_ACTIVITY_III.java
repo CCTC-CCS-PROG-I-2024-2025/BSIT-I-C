edef35101749b8c557e24a94d6b14667ec7c1f7c
jonrobn <johnrobinbelvestre14@gmail.com>
2024-12-11 02:54:15 +0800

    Add files via upload

---
+++ Prelim/Activity III/BELVESTRE_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class BELVESTRE_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Robin";
+        int age = 19;
+        double height = 163.5;
+        float weight = 46.2f;
+        char letter = 'a';
+        boolean isStudent = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isStudent);
+    }
+}
