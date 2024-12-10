25fee23c6d8c9b638e32bc0c0f9858aa28d7f60a
Rhylle1 <esdrelonrhylle@gmail.com>
2024-12-10 11:00:07 +0800

    Add files via upload

---
+++ Prelim/Activity II/ESDRELON_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+    class ESDRELON_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        System.out.println("rhylle keirt esdrelon");
+        System.out.println(19);
+        System.out.println(18);
+        System.out.println(19-18);
+        
+    }
+
+    
+}
