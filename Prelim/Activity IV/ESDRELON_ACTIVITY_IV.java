ef94ade1fb10dbafd77e8c903f801b8db35eb052
Rhylle1 <esdrelonrhylle@gmail.com>
2024-12-10 11:01:51 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ESDRELON_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+ class ESDRELON_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age = 19;
+        int height = 167;
+        int weight = 62;
+        int sum = age + height + weight ;
+        int different = age - height -weight;
+        int product = age * height * weight;
+        int quotient = age / height / weight;
+        System.out.println(sum);
+        System.out.println(different);
+        System.out.println(product);
+        System.out.println(quotient);
+        
+    }
+
+    
+}
