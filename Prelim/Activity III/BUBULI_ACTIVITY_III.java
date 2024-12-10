3d0863e548b6c236281e2dcb8db5702f9d6c4321
Yehoshuaaaaa <joshuabubuli21@gmail.com>
2024-12-10 10:57:39 +0800

    Add files via upload

---
+++ Prelim/Activity III/BUBULI_ACTIVITY_III.java
@@ -0,0 +1,21 @@
+class BUBULI_ACTIVITY_III {
+     public static void main(String[] args) {
+    
+    
+        boolean isOpen = false;
+        String name;
+        char letter = 'B';
+        int age = 18;
+        name = "Joshua";
+        double height = 163;
+        float weight = 60.1f;
+        
+        System.out.println(name);
+        System.out.println(isOpen);
+        System.out.println(letter);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+     }
+   }
+   
