e537f2d7623d2a730bc0b6f95bc2fc4d67700613
LYREL N BETUIN <lyrelbetuin@gmail.com>
2024-12-10 10:44:03 +0800

    Delete Prelim/Activity II/BETUINACTIVITYII.java

--- Prelim/Activity II/BETUINACTIVITYII.java
+++
@@ -1,9 +0,0 @@
-public class BETUIN_ACTIVITY_II{ 
-
-    public static void main(String[] args) {
-        System.out.println("Lyrel N. Betuin");  
-        System.out.println("18");               
-        System.out.println("19");              
-        System.out.println(19-18);                          
-    } 
-}
