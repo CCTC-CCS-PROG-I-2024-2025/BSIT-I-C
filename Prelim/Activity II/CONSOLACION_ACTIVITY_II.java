66821c627eda0b62cd574007d535700799fc09e3
SandraMaeConsolacion <sandramaeconsolacion3@gmail.com>
2024-12-10 11:12:13 +0800

    Add files via upload

---
+++ Prelim/Activity II/CONSOLACION_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class CONSOLACION_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Sandra Mae Consolacion");
+        System.out.println(18);
+        System.out.println(17);
+        System.out.println(18-17);
+    }
+}
