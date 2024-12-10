3a90c8c3f905d3b0c1cfb0ae3ef021ca77b2d15a
Jansen Sagaysay <jansensagaysay241@gmail.com>
2024-12-10 11:37:49 +0800

    Add files via upload

---
+++ Prelim/Activity III/SAGAYSAY_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+ class SAGAYSAY_ACTIVITY_III {
+    public static void main(String[] args){
+        String name = "jansen";
+        int age = 18;
+        double height = 1.32;
+        float weight = 46.1f;
+        char letter = 'j';
+        boolean isClose = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isClose);
+    }
+}
