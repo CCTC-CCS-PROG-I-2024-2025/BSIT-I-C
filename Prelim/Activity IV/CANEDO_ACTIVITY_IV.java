1c525ddc602f849cca47971c299e56e42d89319b
JamesdavidCanedo <jmsmithcanedo@gmail.com>
2024-12-10 11:10:51 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CANEDO_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class CANEDO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        int c = 10;
+        
+        int sum = a+b+c;
+        int difference = a-b-c;
+        int product = a*b*c;
+        int qoutient = a/b/c;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(qoutient);
+    }
+
+}   
