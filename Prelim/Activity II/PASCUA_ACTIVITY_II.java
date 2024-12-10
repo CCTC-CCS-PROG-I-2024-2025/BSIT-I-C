2a67a6e810211a5addf933a6dbb3414e814d2f63
lorejeanpascua <lorejeanpascua@gmail.com>
2024-12-10 10:49:21 +0800

    Add files via upload

---
+++ Prelim/Activity II/PASCUA_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class PASCUA_ACTIVITY_II {
+    public static void main (String[] args){
+        System.out.println("Lore Jean Pascua");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18=========================================================================);
+    }
+}
