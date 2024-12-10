570fd47a6f789f8ae5fe05c431a390e3e1cb776c
pyangmerca <piamerca88@gmail.com>
2024-12-10 11:21:22 +0800

    Add files via upload

---
+++ Prelim/Activity III/MERCA_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+public class MERCA_ACTIVITY_III {
+    public static void main (String[] args) {
+        String name = "zopia";
+        int age = 18;
+        double weight = 43.3;
+        float cool = 1.54f;
+        char wool = 'B';
+        boolean isCat = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(cool);
+        System.out.println(wool);
+        System.out.println(isCat);
+    }
+ }{
+
+}
