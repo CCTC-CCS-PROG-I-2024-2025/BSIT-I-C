41c5798586371fb5fb5d4d3b9e3295d8c2b2a1d3
kuzeeexyz <junrelterobias123@gmail.com>
2024-12-10 10:52:42 +0800

    Add files via upload

---
+++ Prelim/Activity II/TEROBIAS_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+public class TEROBIAS_ACTIVITY_II {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 23;
+        int c = 50;
+        
+        int sum = a+b+c;
+        int difference = a - b - c;
+        int product = a * b * c;
+        int quotient = a / b / c;
+
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
