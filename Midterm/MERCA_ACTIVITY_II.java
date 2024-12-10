b318b0e86a024a347878d93b057e2dd1c5d508a0
pyangmerca <piamerca88@gmail.com>
2024-12-10 10:51:21 +0800

    Delete Midterm/MERCA_ACTIVITY_II.java

--- Midterm/MERCA_ACTIVITY_II.java
+++
@@ -1,8 +0,0 @@
-class MERCA_ACTIVITY_II {
-    public static void main(String[] args) {
-        System.out.println("Zopia Mae V. Merca");
-        System.out.println(18);
-        System.out.println(20);
-        System.out.println(18 - 20);
-    }
-}
