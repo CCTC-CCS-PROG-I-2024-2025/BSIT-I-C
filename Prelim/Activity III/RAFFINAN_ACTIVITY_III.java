4714a8748b27068642c8ccb5ae44103778a006b7
Helowwww <ravikayle01@gmail.com>
2024-12-11 19:46:18 +0800

    Add files via upload

---
+++ Prelim/Activity III/RAFFINAN_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+public class RAFFINAN_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = ("RAVIKYLE RAFFINAN");
+        System.out.println(name);
+        int score = (25);
+        System.out.println(score);
+        double height = (154.94);
+        System.out.println(height);
+        float price = (154.94f);
+        System.out.println(price);
+        char answer = ('B');
+        System.out.println(answer);
+        Boolean open = (true);
+        System.out.println(open);
+    
+    }
+
+}
