325bb249051c88d95d30e7a8bb39aada061fb808
khattyybanez <ybanezkhatty1@gmail.com>
2024-12-11 15:12:15 +0800

    Add files via upload

---
+++ Prelim/Activity II/YBANEZ_ACTIVITY_ll.java
@@ -0,0 +1,8 @@
+class YBANEZ_ACTIVITY_II {
+    public static void main (String[] args) {
+      System.out.println("Khatty YBANEZ");
+      System.out.println(19);
+      System.out.println(25);
+      System.out.println(19-25);
+    }
+  }
