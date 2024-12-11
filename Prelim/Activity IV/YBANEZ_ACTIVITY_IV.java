84161d9a48d3f8f1a43346efcf8e581df4281f2b
khattyybanez <ybanezkhatty1@gmail.com>
2024-12-11 15:20:39 +0800

    Add files via upload

---
+++ Prelim/Activity IV/YBANEZ_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+public class YBANEZ_ACTIVITY_IV{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30; 
+      
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+        
+
+
+    }
+}
