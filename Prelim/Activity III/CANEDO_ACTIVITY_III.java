7e7adecaef85a283f3e4618f2f422876e0fb22b3
nathanieljames1 <canedonathaniel21@gmail.com>
2024-12-11 16:05:58 +0800

    Add files via upload

--- Prelim/Activity III/CANEDO_ACTIVITY_III.java
+++ Prelim/Activity III/CANEDO_ACTIVITY_III.java
@@ -1,17 +1,16 @@
 public class CANEDO_ACTIVITY_III {
-public static void main(String[] args) {
-    String name = "James David";
-    int age = 17;
-    double height = 5.4;
-    float weight = 45.1f;
-    char letter = 'J';
-    boolean isStudent = true;
-    
-    System.out.println(name);
-    System.out.println(age);
-    System.out.println(height);
-    System.out.println(weight);
-    System.out.println(letter);
-    System.out.println(isStudent);
+    public static void main(String[] args){
+        String name = "Nathaniel James";
+        int age = 19;
+        double height = 5.3;
+        float weight = 60.1f;
+        char letter = 'N';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+    }
 }
-        }
