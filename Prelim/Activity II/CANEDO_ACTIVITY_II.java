8bbece6249bad4c1f88333598e0f38e190cdc9a3
nathanieljames1 <canedonathaniel21@gmail.com>
2024-12-11 16:02:27 +0800

    Add files via upload

--- Prelim/Activity II/CANEDO_ACTIVITY_II.java
+++ Prelim/Activity II/CANEDO_ACTIVITY_II.java
@@ -1,8 +1,8 @@
-class CANEDO_ACTIVITY_II {
-public static void main(String[] args) {
-    System.out.println("James David Canedo");
-    System.out.println(17);
-    System.out.println(18);
-    System.out.println(17 - 18); 
-}   
+public class CANEDO_ACTIVITY_II {
+    public static void main(String[] args){
+        System.out.println("Nathaniel james Canedo");
+        System.out.println("19");
+        System.out.println(17);
+        System.out.println(19-17);
+    }    
 }
