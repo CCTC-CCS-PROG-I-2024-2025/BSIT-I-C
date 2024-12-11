14237f9b3d6eaf467e34b6f9df1ab300681ab85b
khattyybanez <ybanezkhatty1@gmail.com>
2024-12-11 15:17:04 +0800

    Add files via upload

---
+++ Prelim/Activity III/YBANEZ_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class YBANEZ_ACTIVITY_III {
+    public static void main (String []args) {
+        String name = "Khatty";
+        System.out.println(name);
+        int age = 19;
+        System.out.println(age);
+        char letter = 'K';
+        System.out.println(letter);
+        boolean isOpen = false;
+        System.out.println(isOpen);
+        double weight = 51;
+        System.out.println(weight);
+        float height = 156.5f;
+        System.out.println(height);
+
+    }
+}
