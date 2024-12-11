7fe5329862fbc7f23e0802132ab8a048e6c0ec7f
Helowwww <ravikayle01@gmail.com>
2024-12-11 19:43:36 +0800

    Add files via upload

---
+++ Prelim/Activity II/RAFFINAN_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class RAFFINAN_ACTIVITY_II {
+    public static void main (String[] args){
+        System.out.println("RAFFINAN RAVIKYLE");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+    }
+}
