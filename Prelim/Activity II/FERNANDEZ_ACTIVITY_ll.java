b7b1c825cc8828443fe142d7b77a2f206327690d
daevonfernandez <fernandezdaevon49@gmail.com>
2024-12-11 16:22:14 +0800

    Add files via upload

---
+++ Prelim/Activity II/FERNANDEZ_ACTIVITY_ll.java
@@ -0,0 +1,8 @@
+class FERNANDEZ_ACTIVITY_ll {
+    public static void main (String [] args) {
+        System.out.println("Christian John Blanco");
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(18-19);
+    }
+}
