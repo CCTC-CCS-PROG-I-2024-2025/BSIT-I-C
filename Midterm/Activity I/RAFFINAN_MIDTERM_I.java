3943cbd007283f6814f1e1f1bcda17b24b69d0f6
Helowwww <ravikayle01@gmail.com>
2024-12-11 19:54:37 +0800

    Add files via upload

---
+++ Midterm/Activity I/RAFFINAN_MIDTERM_I.java
@@ -0,0 +1,15 @@
+public class RAFFINAN_MIDTERM_I {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Enter a color");
+        String color = sc.nextLine();
+         
+
+        switch (color) {
+            case " red " :
+              System.out.println(1);
+              break;
+            case " green " :
+              System.out.println(2);
+              break;
+            case " black ":
