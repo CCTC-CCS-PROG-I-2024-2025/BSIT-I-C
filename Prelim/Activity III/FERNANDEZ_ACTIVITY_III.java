10069e67657eebe17777b6f55230d87afc2c7d38
daevonfernandez <fernandezdaevon49@gmail.com>
2024-12-11 16:24:16 +0800

    Add files via upload

---
+++ Prelim/Activity III/FERNANDEZ_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class FERNANDEZ_ACTIVITY_III {
+    public static void main (String[] args){
+        String name = "Christian";
+        int age = 18;
+        double height = 152.4f;
+        char surename = 'F';
+        boolean isnotloyal = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(surename);
+        System.out.println(isnotloyal);
+    }
+}
