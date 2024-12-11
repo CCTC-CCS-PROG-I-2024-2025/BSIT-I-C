b18900d138aaff96db30f9eb9ad3599b61875474
daevonfernandez <fernandezdaevon49@gmail.com>
2024-12-11 16:27:28 +0800

    Add files via upload

---
+++ Prelim/Activity IV/FERNANDEZ_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+class FERNANDEZ_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        int a = 18;
+        int sum = x + y + a;
+        int difference = x - y - a;
+        int product = x * y * a;
+        int quotient = x / y / a;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
