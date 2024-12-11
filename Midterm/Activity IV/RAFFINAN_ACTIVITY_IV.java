ff9ad10d832d1237d6403325f324cb4786826977
Helowwww <ravikayle01@gmail.com>
2024-12-11 20:07:30 +0800

    Add files via upload

---
+++ Midterm/Activity IV/RAFFINAN_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RAFFINAN_ACTIVITY_IV {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a non-negative interger");
+        int x = sc.nextInt();
+
+        int factorial = 1;
+        for ( int j = 1; j <= x; j++ ){
+            factorial *=j;
+        }
+        System.out.println("The factorial of" + x + "is:" + factorial);
+    }
+}
