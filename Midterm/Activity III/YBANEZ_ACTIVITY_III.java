016c7369512e7ec2ba0d316fd1ab7daf3deebbb7
khattyybanez <ybanezkhatty1@gmail.com>
2024-12-12 12:09:04 +0800

    Update YBANEZ_ACTIVITY_III.java

--- Midterm/Activity III/YBANEZ_ACTIVITY_III.java
+++ Midterm/Activity III/YBANEZ_ACTIVITY_III.java
@@ -1,9 +1,9 @@
 public class YBANEZ_ACTIVITY_III {
     public static void main(String[] args) {
         String name="Renelyn";
-        int age=18;
-        double height=1.49;
-        char letter='A'; 
+        int age=19;
+        double height=1.56;
+        char letter='K'; 
         boolean isStudent=true;
         System.out.println(name);
         System.out.println(age);
