09ddc6ff7ac46f4c2b024215c7f630eb77d77afc
aliahbethmahinay <aliahbethmahinay@gmail.com>
2024-12-12 12:10:08 +0800

    Update and rename MAHINAY_ACTIVITY_III.java to MAHINAY_A_ACTIVITY_III.java

--- Midterm/Activity III/MAHINAY_A_ACTIVITY_III.java
+++ Midterm/Activity III/MAHINAY_A_ACTIVITY_III.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class MAHINAY_ACTIVITY_III {
+public class MAHINAY_A_ACTIVITY_III {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
