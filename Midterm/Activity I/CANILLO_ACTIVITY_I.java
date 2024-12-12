ef9dcab7a062a06a6498682aaf1752772816897d
Karess Sheen Canillo <karesssheenc@gmail.com>
2024-12-12 21:53:47 +0800

    Add files via upload

---
+++ Midterm/Activity I/CANILLO_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CANILLO_ACTIVITY_I {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the color of the item:");
+        String color = sc.nextLine();
+
+        switch (color) {
+            case "red":
+                System.err.println(1);
+                break;
+            case "green":
+                System.out.println(2);
+                break;
+            case "blue":
+                System.out.println(3);
+                break;
+        }
+    }
+}
