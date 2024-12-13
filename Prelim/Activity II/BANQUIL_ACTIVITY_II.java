99ed7fb232707b9b0211f510efb3424baebcf3b6
ptrckjshbnql <ptrckjshbnql@gmail.com>
2024-12-13 22:47:17 +0800

    Add files via upload

---
+++ Prelim/Activity II/BANQUIL_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class BANQUIL_ACTIVITY_II {
+    public static void main (String[] args) {
+      System.out.println("Patrick Joshua BANQUIL");
+      System.out.println(25);
+      System.out.println(23);
+      System.out.println(25-23);
+    }
+  }
