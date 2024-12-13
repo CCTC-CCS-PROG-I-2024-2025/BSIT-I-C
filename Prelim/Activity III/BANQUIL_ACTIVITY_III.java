42abbf1309a1276bc8ac65a4b10abd671bc813ca
ptrckjshbnql <ptrckjshbnql@gmail.com>
2024-12-13 22:47:52 +0800

    Add files via upload

---
+++ Prelim/Activity III/BANQUIL_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class BANQUIL_ACTIVITY_III {
+    public static void main (String []args) {
+        String name = "Patrick";
+        System.out.println(name);
+        int age = 25;
+        System.out.println(age);
+        char letter = 'P';
+        System.out.println(letter);
+        boolean isOpen = false;
+        System.out.println(isOpen);
+        double weight = 64;
+        System.out.println(weight);
+        float height = 170.5f;
+        System.out.println(height);
+
+    }
+}
