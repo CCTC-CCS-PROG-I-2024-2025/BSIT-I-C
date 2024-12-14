ec0a90c2e378d0e0571e3cfa1da40e8b22c1edee
Alyanah Belle Bacunawa <alyanahbelleonobacunawa@gmail.com>
2024-12-14 12:10:27 +0800

    Add files via upload

---
+++ Midterm/Activity IV/BACUNAWA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BACUNAWA_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter non negative integer number");
+        int num = sc.nextInt();
+         
+        int factorial = 1;
+        
+        for(int x =1; x <=num; x++){
+            factorial *=x;
+            System.out.println(factorial);
+        }
+    }
+}
