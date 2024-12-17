e80537481d759ee58471405d21e89489210f0e81
Jerica Mahinay <jericaledesma29@gmail.com>
2024-12-17 21:23:18 +0800

    Add files via upload

---
+++ Prelim/Activity II/MAHINAY_J_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+public class MAHINAY_J_ACTIVITY_II {
+    public static void main (String [] args) {
+
+
+        System.out.println("Jerica Mahinay");
+        System.out.println(28);
+        System.out.println(18);
+        System.out.println(28-18);
+    }
+}
