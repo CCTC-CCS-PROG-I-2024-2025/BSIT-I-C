eb69cf7d66be16dee906b8a353282dac83abd0a0
Jerica Mahinay <jericaledesma29@gmail.com>
2024-12-17 21:23:55 +0800

    Add files via upload

---
+++ Prelim/Activity III/MAHINAY_J_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+public class MAHINAY_J_ACTIVITY_III {
+    public static void main(String [] args) {
+        String name = "Jerica";
+        int age = 28;
+        double height = 152.5;
+        char surname = 'M';
+        boolean isStudent = true;
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(surname);
+        System.out.println(isStudent);
+
+
+
+    }
+    
+}
