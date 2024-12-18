6600019a110f2ba05ace784ac57166db0aeb9f2f
Toni Kristal Rojo <junrelterobias6@GMAIL.COM>
2024-12-18 09:05:15 +0800

    Update ROJO_ACTIVITY_II.java

--- Prelim/Activity II/ROJO_ACTIVITY_II.java
+++ Prelim/Activity II/ROJO_ACTIVITY_II.java
@@ -1,7 +1,7 @@
 public class ROJO_ACTIVITY_II { 
 
     public static void main(String[] args) {
-        System.out.println("Toni Kristal H. Rojo");  
+        System.out.println("Toni Kristal Rojo");  
         System.out.println("18");               
         System.out.println("19");              
         System.out.println(19-18);                          
