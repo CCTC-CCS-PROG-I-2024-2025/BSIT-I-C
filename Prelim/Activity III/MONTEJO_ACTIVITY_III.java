aaf1c6ebea5536c63251a50159f520b78ea983e4
xaviermontejo <xaviermontejo24@gmail.com>
2024-12-25 23:21:24 +0800

    Add files via upload

---
+++ Prelim/Activity III/MONTEJO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+public class MONTEJO_ACTIVITY_III {
+    public static void main(String[] args){
+        String name = "Xavier";
+        int age = 18;
+        double height = 5.2;
+        float weight = 64f;
+        char letter = 'X';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+    }
+}
