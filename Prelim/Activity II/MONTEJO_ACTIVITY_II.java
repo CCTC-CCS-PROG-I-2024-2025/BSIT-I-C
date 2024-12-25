15b86309843c7fa4e766d336bc1714ba74fa57ec
xaviermontejo <xaviermontejo24@gmail.com>
2024-12-25 23:11:22 +0800

    Add files via upload

---
+++ Prelim/Activity II/MONTEJO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class MONTEJO_ACTIVITY_II {
+    public static void main(String[] args){
+        System.out.println("Xavier Montejo");
+        System.out.println("18");
+        System.out.println(17);
+        System.out.println(18-17);
+    }    
+}
