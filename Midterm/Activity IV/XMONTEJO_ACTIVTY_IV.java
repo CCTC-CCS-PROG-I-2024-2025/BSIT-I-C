62a22cbae2643b83a12e9af550666898735b0ebf
xaviermontejo <xaviermontejo24@gmail.com>
2024-12-25 23:38:18 +0800

    Add files via upload

---
+++ Midterm/Activity IV/XMONTEJO_ACTIVTY_IV.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class XMONTEJO_ACTIVITY_IV {
+    public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+        System.out.println("Enter a non-integer number:");
+        int number = sc.nextInt();
+        int factorial = 1;
+        
+        for(int i = 1; i <= number; i++){
+            factorial *= i;
+        
+        } System.out.println("The factorial of " + number + " is " + factorial);
+    }
+    }
+
+
+    
+}
