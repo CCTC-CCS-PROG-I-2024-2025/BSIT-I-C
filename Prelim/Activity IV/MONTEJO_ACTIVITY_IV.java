743345492088c9a6391920d9c7263c269e01da95
xaviermontejo <xaviermontejo24@gmail.com>
2024-12-25 23:26:10 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MONTEJO_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+public class MONTEJO_ACTIVITY_IV {
+        public static void main(String[] args){
+            int a = 6;
+            int b = 8;
+            int c = 10;
+            int sum = a+b+c;
+            int diff = a-b-c;
+            int prod = a*b*c;
+            int qoutient = a/b/c;
+            System.out.println(sum);
+            System.out.println(diff);
+            System.out.println(prod);
+            System.out.println(qoutient);
+        }
+    }
