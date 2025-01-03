6db9a1821441aea6a5900e25eb5735e89a33521f
KobeMGamboa <kobegamboa59@gmail.com>
2025-01-03 13:01:25 +0800

    Add files via upload

---
+++ Midterm/GAMBOA_ACTIVITY_III.java
@@ -0,0 +1,10 @@
+public class GAMBOA_ACTIVITY_III {
+    public static void main (String [] args){
+        int x = 10;
+        while(x >=0){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
+
