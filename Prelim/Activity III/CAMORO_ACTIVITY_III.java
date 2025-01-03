6628ef74333695110b6d12be86054ec31a1b89d2
ninolou123 <harshstein243@gmail.com>
2025-01-03 19:50:16 +0800

    Add files via upload

---
+++ Prelim/Activity III/CAMORO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+public class CAMORO_ACTIVITY_III {
+    public static void main(String[] args){
+        String name = "NINOLOU";
+        int age = 18;
+        double height = 5.1;
+        float weight = 40.1f;
+        char letter = 'N';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+    }
+}
