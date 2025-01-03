101ad59e4f067d5e7cc1f8cac0ecdbd045fabd52
ninolou123 <harshstein243@gmail.com>
2025-01-03 19:50:47 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CAMORO_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+public class CAMORO_ACTIVITY_IV {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int sum = a+b+c;
+        int diff = a-b-c;
+        int prod = a*b*c;
+        int qoutient = a/b/c;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qoutient);
+    }
+}
