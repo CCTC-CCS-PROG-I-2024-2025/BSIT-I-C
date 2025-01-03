a31e5add7dece0a9f0399bb4f2ee2c1bde990851
ninolou123 <harshstein243@gmail.com>
2025-01-03 19:49:51 +0800

    Add files via upload

---
+++ Prelim/Activity II/CAMORO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class CAMORO_ACTIVITY_II {
+    public static void main(String[] args){
+        System.out.println("Ninolou Camoro");
+        System.out.println("20");
+        System.out.println(19);
+        System.out.println(18-17);
+    }    
+}
