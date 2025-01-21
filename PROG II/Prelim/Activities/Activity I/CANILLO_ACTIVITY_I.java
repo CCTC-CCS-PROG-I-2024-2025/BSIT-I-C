ed7be323202b4ac7f0306125e1f8e62e5397649b
Karess Sheen Canillo <karesssheenc@gmail.com>
2025-01-21 11:03:51 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/CANILLO_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+
+class CANILLO_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String [] names=new String[5];
+
+        for(int i =0; i<5; i++) {
+            System.out.println("ENTER NAME" + (i+1) + ":");
+            names [i] = sc.nextLine();
+        }
+        System.out.println("ENTER A NUMBER");
+        int number = sc.nextInt();
+        
+        System.out.println(names[number - 1]); 
+
+        }
+
+
+    }
