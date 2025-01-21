9adbe8ee564a72f39db5d0aab4401a8a40354d4f
AIDEZEIR MELGAR <aidezeirpelderosmelgar@gmail.com>
2025-01-21 10:51:33 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/MELGAR_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class MELGAR_ACTIVITY_I {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Enter name" + (i + 1 ) + ":");
+            names[i] = sc.nextLine();
+
+        }
+        System.out.println("Enter a name that corresponds to the name you want to display");
+        int number = sc.nextInt();
+        System.out.println(names[number-1]);
+    }
+}
+
+
