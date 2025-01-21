f42e2d4b1d255dedff96d8b0186ecf2055df6721
KobeMGamboa <kobegamboa59@gmail.com>
2025-01-21 11:00:44 +0800

    GAMBOA_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/GAMBOA_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/GAMBOA_ACTIVITY_I.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 class GAMBOA_ACTIVITY_I {
-    public static void (String[] args){
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
 
         String[] names = new String[5];
 
         for(int i = 0; i < 5; i++){
-            System.out.printlin("Enter name " + (i + 1) +":");
+            System.out.println("Enter name " + (i + 1) +":");
             names[i] = sc.nextLine();
         }
    
-   System.out.printlin(x:"Enter a number that corresponds to the name you want to display:");
+   System.out.println(x:"Enter a number that corresponds to the name you want to display:");
    int number = sc.nextInt();
 
-   System.out.printlin(names[number - 1]);
+   System.out.println(names[number - 1]);
     }
 }
