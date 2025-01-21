d190affb622f70c886e50e60bba6b0f49f7dac84
khattyybanez <ybanezkhatty1@gmail.com>
2025-01-21 10:33:48 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/YBANEZ_ACTIVITY.I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class  YBANEZ_ACTIVITY_I{
+
+    public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+
+
+        String[] names = new String [5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Enter name" + (i + 1) + ":");
+
+            names [i] = sc.nextLine();
+
+        }
+
+
+        System.out.println("Enter Number 0-4");
+        int number = sc.nextInt();
+        System.out.println(names[number]);
+    }
+}
