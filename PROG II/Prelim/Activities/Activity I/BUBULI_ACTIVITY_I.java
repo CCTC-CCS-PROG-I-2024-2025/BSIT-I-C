984c6727efb15370157c6cb34507529da811dc57
Yehoshuaaaaa <joshuabubuli21@gmail.com>
2025-01-21 10:27:00 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/BUBULI_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BUBULI_ACTIVITY_I {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      String[] names = new String[5];
+
+      for (int i = 0; i < 5; i++) {
+          System.out.println("Enter name" + (i + 1) + ":");
+          names[i] = sc.nextLine();
+      }
+      System.out.println("Enter a user number");
+      int number = sc.nextInt();
+
+      System.out.println(names[number - 1]);
+    }
+}
