533a15a39ea57e04c4b6af8f9a8a659781a8194c
johnEvangelista <evangelistajohn356@gmail.com>
2025-01-21 10:45:38 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/EVANGELISTA_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class EVANGELISTA_ACTIVITY_I{
+    public static void main(String[] args){
+    Scanner sc = new Scanner (System.in);
+    String [] names = new String [5];
+
+    System.out.println("Enter the name you would like to have");
+
+    for (int i= 0; i < 4; i++){
+         System.out.println("Enter name" + (i + 1) + ":");
+         
+         names [i] = sc.nextLine();
+
+        }
+        System.out.println("Enter a number that corresponds to the name you want to display");
+        int num = sc.nextInt();
+
+        System.out.println(names[num - 1]);
+    }
+}
