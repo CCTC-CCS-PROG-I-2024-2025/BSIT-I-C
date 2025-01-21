48c8e2d2b10eb9fca09a08a5145cd3ec9c548099
Syrell Jane S. Dacuyan <xyriljn@gmail.com>
2025-01-21 10:33:30 +0800

    Add files via upload

--- PROG II/Prelim/Activities/Activity I/DACUYAN_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/DACUYAN_ACTIVITY_I.java
@@ -7,29 +7,17 @@ Scanner sc = new Scanner(System.in);
 
     String[] names = new String[5];
 
-    for (int i = 0; i < 5; i++){
-        System.out.print("Enter name "+ (i + 1) + ":");
-         names[i] = scanner.nextLine();
-          int index = -1;
-        while (index < 0 || index >= 5) {
-            System.out.print("Enter a number between 1 and 5 to display the corresponding name: ");
-
-            if (scanner.hasNextInt()) {
-                index = scanner.nextInt() - 1;
-
-                if (index < 0 || index >= 5) {
-                    System.out.println("Invalid number! Please enter a number between 1 and 5.");
-                }
-            } else {
-                System.out.println("Invalid input! Please enter a valid number between 1 and 5.");
-                scanner.next();
-            }
+    
+        System.out.println("Enter 5 names:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name" +  (i + 1) + ": ");
+            names[i] = scanner.nextLine();
         }
 
-        System.out.println("The name selected is: " + names[index]);
-        scanner.close();
-    }
-    }
-    
-    }
+        
+            System.out.print("Enter a number that corresponds to the name you want to display: ");
+            
+                int number = scanner.nextInt();
+                System.out.println (names [ number-1]);
+
 
