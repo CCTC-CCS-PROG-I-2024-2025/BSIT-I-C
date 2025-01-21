b2156582cb882536cb58e17f72d6838e54f686ec
Stephen Jay Bulanon <stephenjaydonwiser@gmail.com>
2025-01-21 11:01:47 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/BULANON_ACTIVITY_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class BULANON_ACTIVITY_I{ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Etername name" +(i + 1)+ ":");
+
+            name [i] = sc.nextline();
+
+        }
+
+
+        system.out.printed("Enter Number 0-4");
+        int number = sc.nextInt();
+        System.out.println(names[number]);
+
+
+ 
+    }
+
+}
