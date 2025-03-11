05f7aba5e64d0eadc6e6fc6c652d69093ce1a49c
JamesdavidCanedo <jmsmithcanedo@gmail.com>
2025-03-11 10:43:08 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CANEOD_D_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class CANEOD_D_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int x = 7;
+        int z = 3;
+        int p = 9;
+        
+        int sum = x+z+p;
+        int difference = x-z-p;
+        int product = x*z*p;
+        int qoutient = x/z/p;
+        
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(qoutient);
+    }
+}
