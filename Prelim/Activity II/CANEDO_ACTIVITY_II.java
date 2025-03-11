e16cc28e0a68bf7007f72621c3440d2149f1a590
JamesdavidCanedo <jmsmithcanedo@gmail.com>
2025-03-11 10:36:44 +0800

    Add files via upload

--- Prelim/Activity II/CANEDO_ACTIVITY_II.java
+++ Prelim/Activity II/CANEDO_ACTIVITY_II.java
@@ -1,8 +1,9 @@
 public class CANEDO_ACTIVITY_II {
-    public static void main(String[] args){
-        System.out.println("Nathaniel james Canedo");
-        System.out.println("19");
-        System.out.println(17);
-        System.out.println(19-17);
-    }    
+
+    public static void main(String[] args) {
+        System.out.println("joshua aviles");
+        System.out.println(19);
+        System.out.println(19);
+        System.out.println(19 - 19);
+    }
 }
