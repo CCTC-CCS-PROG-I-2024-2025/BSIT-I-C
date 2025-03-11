ef6f06bfc406ed4775e93a9ae660b4aa786873a8
JamesdavidCanedo <jmsmithcanedo@gmail.com>
2025-03-11 10:44:14 +0800

    Add files via upload

---
+++ Prelim/Activity III/CANEDO_D_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+public class CANEDO_D_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "James David";
+        int age = 19;
+        double height = 5.4;
+        float weight = 60f;
+        char letter = 'j';
+        boolean isstudent = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isstudent);
+    }
+}
