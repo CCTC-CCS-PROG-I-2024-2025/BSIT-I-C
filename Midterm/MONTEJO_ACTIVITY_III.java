b74738a3d3d77b93f741f114eb4eff736702eced
xaviermontejo <xaviermontejo24@gmail.com>
2025-04-10 22:45:06 +0800

    Add files via upload

---
+++ Midterm/MONTEJO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MONTEJO_ACTIVITY_III {
+    public static void main(String [] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter number of second: ");
+         int num = sc.nextInt();
+
+         while(num>=0){
+            System.out.println(num);
+            num--;
+        }
+        sc.close();
+    }
+
+}
